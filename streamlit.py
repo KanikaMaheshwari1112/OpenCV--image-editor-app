4e1f60a4ca10a3c7a715773276b7aab811a07fdb
Kanika Maheshwari <22ds1000263@student.onlinedegree.iitm.ac.in>
2023-01-21 14:10:09 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -19,5 +19,28 @@ st.header('Input any image')
 
 st.file_uploader(label='upload image', type=['png', 'jpg'], accept_multiple_files=True, label_visibility="visible")
 
-sketch= model.load_model('OpenCV_Pencil_sketch_from_image.ipynb')
-st.write("Pencil Sketch of the image:", sketch)
+# Read the image and convert it into an array
+img = cv2.imread("/content/original_image.jpg")
+
+# Filters
+grey_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)            # Gray
+invert_image = cv2.bitwise_not(grey_img)                    # Invert gray
+blur = cv2.GaussianBlur(invert_image, (23,33), 0)           # Gaussian Blur GaussianBlur(source, kernel size, standard dev wrt X)
+invert_blur = cv2.bitwise_not(blur)                         # Invert blur
+pencil = cv2.divide(grey_img, invert_blur, scale = 256.0)   # Blend gray with invert blur
+
+
+
+st.write("Pencil Sketch of the image:", pencil)
+
+# Display the final output
+plt.figure(figsize = (15,8))
+plt.subplot(1, 2, 1)
+plt.title("Original Image", color = 'r', fontsize = 15)
+plt.imshow(img)
+plt.axis('off')
+plt.subplot(1, 2, 2)
+plt.title("Pencil Sketch of the Original Image", color = 'k', fontsize = 15)
+plt.imshow(pencil, cmap = 'gray')
+plt.axis('off')
+plt.show()
