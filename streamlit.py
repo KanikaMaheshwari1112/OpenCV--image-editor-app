1bb6a06b46a85fc9e96c509ed5d550dfd6fcb659
Kanika Maheshwari <22ds1000263@student.onlinedegree.iitm.ac.in>
2023-01-22 19:15:48 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -86,70 +86,70 @@ else:
                     file_name="watercolorsketch.png",
                     mime="image/png"
                 )
-   elif filter== 'Black and White':
-      final_sketch= grayimage(np.array(input_img))
-      im_pil = Image.fromarray(final_sketch)
-      st.success('Hurrah!!! Your Black and White Sketch is ready.')
-      st.balloons()
-      col1, col2 = st.columns(2)
-      with col1:
-        st.header("Original Image")
-        st.image(load_an_image(image_file), width=250)
+  elif filter== 'Black and White':
+     final_sketch= grayimage(np.array(input_img))
+     im_pil = Image.fromarray(final_sketch)
+     st.success('Hurrah!!! Your Black and White Sketch is ready.')
+     st.balloons()
+     col1, col2 = st.columns(2)
+     with col1:
+       st.header("Original Image")
+       st.image(load_an_image(image_file), width=250)
   
-      with col2:
-        st.header("Black and White Sketch")
-        st.image(im_pil, width=250)
-        buf = BytesIO()
-        img = im_pil
-        img.save(buf, format="JPEG")
-        byte_im = buf.getvalue()
-        st.download_button(
+     with col2:
+       st.header("Black and White Sketch")
+       st.image(im_pil, width=250)
+       buf = BytesIO()
+       img = im_pil
+       img.save(buf, format="JPEG")
+       byte_im = buf.getvalue()
+       st.download_button(
                     label="Download sketch",
                     data=byte_im,
                     file_name="blackandwhite.png",
                     mime="image/png"
                 )
-   elif filter== 'Pencil Sketch':
-      final_sketch= pencilsketch(np.array(input_img))
-      im_pil = Image.fromarray(final_sketch)
-      st.success('Hurrah!!! Your Pencil Sketch is ready.')
-      st.balloons()
-      col1, col2 = st.columns(2)
-      with col1:
-        st.header("Original Image")
-        st.image(load_an_image(image_file), width=250)
+  elif filter== 'Pencil Sketch':
+     final_sketch= pencilsketch(np.array(input_img))
+     im_pil = Image.fromarray(final_sketch)
+     st.success('Hurrah!!! Your Pencil Sketch is ready.')
+     st.balloons()
+     col1, col2 = st.columns(2)
+     with col1:
+       st.header("Original Image")
+       st.image(load_an_image(image_file), width=250)
   
-      with col2:
-        st.header("Pencil Sketch")
-        st.image(im_pil, width=250)
-        buf = BytesIO()
-        img = im_pil
-        img.save(buf, format="JPEG")
-        byte_im = buf.getvalue()
-        st.download_button(
+     with col2:
+       st.header("Pencil Sketch")
+       st.image(im_pil, width=250)
+       buf = BytesIO()
+       img = im_pil
+       img.save(buf, format="JPEG")
+       byte_im = buf.getvalue()
+       st.download_button(
                     label="Download sketch",
                     data=byte_im,
                     file_name="pencilsketch.png",
                     mime="image/png"
                 )   
-    elif filter== 'Blur Effect':
-      final_sketch= blurimage(np.array(input_img))
-      im_pil = Image.fromarray(final_sketch)
-      st.success('Hurrah!!! Your Blur Image is ready.')
-      st.balloons()
-      col1, col2 = st.columns(2)
-      with col1:
-        st.header("Original Image")
-        st.image(load_an_image(image_file), width=250)
+   elif filter== 'Blur Effect':
+     final_sketch= blurimage(np.array(input_img))
+     im_pil = Image.fromarray(final_sketch)
+     st.success('Hurrah!!! Your Blur Image is ready.')
+     st.balloons()
+     col1, col2 = st.columns(2)
+     with col1:
+       st.header("Original Image")
+       st.image(load_an_image(image_file), width=250)
   
-      with col2:
-        st.header("Blur Effect Image")
-        st.image(im_pil, width=250)
-        buf = BytesIO()
-        img = im_pil
-        img.save(buf, format="JPEG")
-        byte_im = buf.getvalue()
-        st.download_button(
+     with col2:
+       st.header("Blur Effect Image")
+       st.image(im_pil, width=250)
+       buf = BytesIO()
+       img = im_pil
+       img.save(buf, format="JPEG")
+       byte_im = buf.getvalue()
+       st.download_button(
                     label="Download sketch",
                     data=byte_im,
                     file_name="blurimage.png",
